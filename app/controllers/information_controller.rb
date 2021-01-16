de0eb1b0fadd840d4fa021b03f3fd5b20e7edd78
benny <benny08118@gmail.com>
2021-01-16 23:45:33 +0800

    info create done

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,5 +1,23 @@
 class InformationController < ApplicationController
+  def index
+    @info=Information.first
+  end
   def new 
-      @info=Information.new
+    @info=Information.new
   end    
+  def create
+    @info=Information.new(info_parama_primet)
+    if @info.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def info_parama_primet
+    params.require(:information).permit(:name,:ename,:picture)
+  end
 end
