bb52ef52f19f48cc753370fba06a4daf75ef9b5a
benny <benny08118@gmail.com>
2021-01-20 14:15:40 +0800

    fix program bar delay show

--- app/forntend/controllers/programbar_controller.js
+++ app/forntend/controllers/programbar_controller.js
@@ -1,20 +1,22 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-      static targets = ["value"]
-      static values = { bar: String }
-      connect() {
-            let preValue=this.valueTarget.getAttribute("data-value")
-            //alert(preValue)
-            this.valueTarget.setAttribute("value",'0')
-           
-            for(var i=0;i<=preValue;i++){
-           setInterval(()=>{    
-              this.valueTarget.setAttribute("value",`${i}`)
-            },1000*i);
-            }
-            
-            
-          }
-  
+  static targets = ["value"]
+  static values = { bar: String }
+  async connect(event) {
+    let preValue = this.valueTarget.getAttribute("data-value")
+    //alert(preValue)
+    this.valueTarget.setAttribute("value", '0')
+
+    for (var i = 0; i < preValue; i++) {
+        this.valueTarget.setAttribute("value", `${i}`)
+        await sleep(5);
+    }
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    
+
+  }
+
 }
