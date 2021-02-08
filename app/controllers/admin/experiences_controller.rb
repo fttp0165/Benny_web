17361b71821a587eb2bb935bd9eff9ef3b09df1c
benny <benny08118@gmail.com>
2021-02-08 23:50:14 +0800

    add experience in backend ; new is done

--- app/controllers/admin/experiences_controller.rb
+++ app/controllers/admin/experiences_controller.rb
@@ -1,14 +1,20 @@
 class Admin::ExperiencesController < Admin::BaseController
+  before_action :find_experience,only:[:edit,:update]
   def index
-    
+    @exp=Experience.all
   end
 
   def new
-    
+    @exp=Experience.new
   end
 
   def create
-    
+    @exp=Experience.new(exp_params)
+    if @exp.save
+      redirect_to admin_experiences_path,notice:"新增經歷成功"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -16,7 +22,21 @@ class Admin::ExperiencesController < Admin::BaseController
   end
 
   def update
-    
+    if @exp.update(exp_params)
+      redirect_to admin_experiences_path,notice:"更新經歷成功"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_experience
+    @exp=Experience.find(params[:id])
+  end
+
+  def exp_params
+    params.require(:experiences).permit(:name,:location,:start_date,:end_date)
   end
   
 end
