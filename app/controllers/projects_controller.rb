cf5200cfd1f30a7398ac3f346856b841eae1c1b5
benny <benny08118@gmail.com>
2021-02-21 23:37:35 +0800

    create projects list and show

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,9 @@
 class ProjectsController < ApplicationController
+  before_action :find_project,except:[:index,:new,:create]
 
-
+  def index
+    @projects=current_user.projects.all
+  end
   def new
     @project=current_user.projects.new
   end  
@@ -14,19 +17,28 @@ class ProjectsController < ApplicationController
   end
 
   def edit
-    @project=Project.find(params[:id])
+
   end
 
   def update
-    @project=Project.find(params[:id])
+ 
     if @project.update(project_pramas_permit)
       redirect_to root_path
     else
       render :edit
     end
   end
+
+  def show
+    
+  end
   
   private
+  
+  def find_project
+    @project=Project.find(params[:id])
+  end
+
   def project_pramas_permit
       params.require(:project).permit(:project_name,:project_content,:project_picture)
         
